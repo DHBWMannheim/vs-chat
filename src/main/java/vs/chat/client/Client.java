734bf3df7b181e7ccd625df2c89a00ed67e4f269
Troy Kessler <troy_kessler@protonmail.com>
2020-03-09 21:53:12 +0100

    added java swing login gui

--- src/main/java/vs/chat/client/Client.java
+++ src/main/java/vs/chat/client/Client.java
@@ -1,5 +1,10 @@
 package vs.chat.client;
 
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -14,46 +19,74 @@ public class Client {
         String hostname = "localhost";
         PrintWriter networkOut;
         BufferedReader networkIn;
-        Socket socket = null;
 
         try {
-//            while (true) {
-                socket = new Socket(hostname, PORT);
-                networkOut = new PrintWriter(socket.getOutputStream());
-                networkIn = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-//                BufferedReader userIn = new BufferedReader(new InputStreamReader(System.in));
-//                String userInput = userIn.readLine();
-//                if (userInput.equals("/exit")) {
-//                    System.out.println("Exiting...");
-//                    break;
-//                }
-                networkOut.println("LOGIN");
-                networkOut.println("woop:woopToo");
-                networkOut.flush();
-                String response = networkIn.readLine();
-                System.out.println(response);
-                
-                networkOut.println("SEND");
-                networkOut.println("woop:woopToo");
-                networkOut.flush();
+            Socket socket = new Socket(hostname, PORT);
+            networkOut = new PrintWriter(socket.getOutputStream());
+            networkIn = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 
-                
-                
-//                networkOut.println("SEND");
-//                networkOut.println("username:passwort");
-                networkOut.flush();
-//            }
+            // Open GUI
+
+            JFrame mainFrame = new JFrame("DHBW Chat");
+            mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            mainFrame.setSize(500, 700);
+
+            // add elements
+            JPanel mainPanel = new JPanel();
+
+            JLabel usernameLabel = new JLabel("Username:");
+            JLabel passwordLabel = new JLabel("Passwort:");
+
+            JTextField username = new JTextField("", 32);
+            JTextField password = new JPasswordField("", 32);
+
+            JButton loginButton = new JButton("Login");
+
+            loginButton.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    try {
+                        System.out.println(username.getText());
+                        System.out.println(password.getText());
+                        networkOut.println("LOGIN");
+                        networkOut.println(username.getText() + ":" + password.getText());
+
+                        networkOut.flush();
+
+                        String token = networkIn.readLine();
+                        System.out.println(token);
+
+                    } catch (IOException io) {
+                        io.printStackTrace();
+                    }
+                }
+            });
+
+            mainPanel.add(usernameLabel);
+            mainPanel.add(username);
+
+            mainPanel.add(passwordLabel);
+            mainPanel.add(password);
+
+            mainPanel.add(loginButton);
+
+            mainFrame.add(mainPanel);
+
+            mainFrame.addWindowListener(new WindowAdapter() {
+                @Override
+                public void windowClosing(WindowEvent event) {
+                    try {
+                        socket.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+
+            mainFrame.setVisible(true);
 
         } catch (IOException e) {
             e.printStackTrace();
-        } finally {
-            try {
-                if (socket != null) {
-                    socket.close();
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
         }
     }
 
