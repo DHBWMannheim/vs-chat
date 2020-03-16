0c05c746a3dcd4a726a0df567632f6269675c3cb
Jan Grübener <jan.gruebener@gmail.com>
2020-03-16 14:45:18 +0100

    Created Recent Conversation GUI

--- src/main/java/vs/chat/client/UI/Gui.java
+++ src/main/java/vs/chat/client/UI/Gui.java
@@ -1,8 +1,12 @@
-package vs.chat.client;
+package vs.chat.client.UI;
 
+import vs.chat.entities.Message;
+
+import javax.sql.rowset.FilteredRowSet;
 import javax.swing.*;
 import javax.swing.border.EtchedBorder;
 import java.awt.*;
+import java.util.UUID;
 
 public class Gui {
     //TODO: Use material-ui-swing from https://github.com/vincenzopalazzo/material-ui-swing
@@ -19,7 +23,7 @@ public class Gui {
     private static JPanel footer() {
         //TODO: Implement Action Listener for Emoji selection
         JPanel footerPannel = new JPanel(new FlowLayout(FlowLayout.CENTER));
-        ImageIcon imageIcon = new ImageIcon("src/main/java/vs/chat/client/emoji.png");
+        ImageIcon imageIcon = new ImageIcon("src/main/java/vs/chat/client/UI/emoji.png");
         Image image = imageIcon.getImage().getScaledInstance(30, 30, java.awt.Image.SCALE_SMOOTH);
         JLabel emoji = new JLabel(new ImageIcon(image));
         JTextArea messageInput = new JTextArea("Nachricht", 2, 30);
@@ -31,11 +35,11 @@ public class Gui {
     private static JPanel header(String username) {
         //TODO: Implement Action Listener to get back to Recent User view
         JPanel headerPannel = new JPanel(new GridLayout(0, 3));
-        ImageIcon imageIconProfile = new ImageIcon("src/main/java/vs/chat/client/profile.png");
+        ImageIcon imageIconProfile = new ImageIcon("src/main/java/vs/chat/client/UI/profile.png");
         Image imageProfile = imageIconProfile.getImage().getScaledInstance(40, 40, java.awt.Image.SCALE_SMOOTH);
         JLabel profile = new JLabel(new ImageIcon(imageProfile));
 
-        ImageIcon imageIconBack = new ImageIcon("src/main/java/vs/chat/client/back.png");
+        ImageIcon imageIconBack = new ImageIcon("src/main/java/vs/chat/client/UI/back.png");
         Image imageBack = imageIconBack.getImage().getScaledInstance(60, 35, java.awt.Image.SCALE_SMOOTH);
         JLabel profileBack = new JLabel(new ImageIcon(imageBack));
 
@@ -47,13 +51,20 @@ public class Gui {
         return headerPannel;
 
     }
+    private static JScrollPane DisplayMessages(){
+        //TODO: Implementation needed after Chat Entity is provided!
+        return new JScrollPane();
+    }
 
-    private static JPanel displayMessage(String message) {
+    private static JPanel displayNewMessage(Message message) {
         //TODO: Differ between Incoming and Outgoing Message, fix Position
-        JPanel messagePannel = new JPanel(new FlowLayout());
-        JLabel content = new JLabel(message);
-        content.setBorder(BorderFactory.createEtchedBorder(EtchedBorder.RAISED));
+        JPanel messagePannel = new JPanel(new GridLayout(2,0));
+        JLabel userName = new JLabel(message.getOrigin().toString());
+        JLabel content = new JLabel(message.getContent());
+        messagePannel.add(userName);
         messagePannel.add(content);
+        messagePannel.setBorder(BorderFactory.createEtchedBorder());
+
         return messagePannel;
     }
 
@@ -64,31 +75,42 @@ public class Gui {
         return inputPannel;
     }
 
-    private static JPanel displayRecentContacts() {
-        //TODO: Create Border arround each contact
-        JPanel contactsPannel = new JPanel(new GridLayout(0, 2));
-        ImageIcon imageIconProfile = new ImageIcon("src/main/java/vs/chat/client/profile.png");
-        Image imageProfile = imageIconProfile.getImage().getScaledInstance(40, 40, java.awt.Image.SCALE_SMOOTH);
-        JLabel profile = new JLabel(new ImageIcon(imageProfile));
-        JLabel userName = new JLabel("Nutzername");
-        contactsPannel.setBorder(BorderFactory.createEtchedBorder(EtchedBorder.RAISED));
-        contactsPannel.add(profile);
-        contactsPannel.add(userName);
+    private static JPanel displayRecentContacts(String[] userNames) {
+        //TODO: Add Event Listener
+        JPanel contactsPanel = new JPanel(new GridLayout(0, 1));
+        for (String userName : userNames) {
+            ImageIcon imageIconProfile = new ImageIcon("src/main/java/vs/chat/client/UI/profile.png");
+            Image imageProfile = imageIconProfile.getImage().getScaledInstance(40, 40, java.awt.Image.SCALE_SMOOTH);
+            JLabel profile = new JLabel(new ImageIcon(imageProfile));
+            JLabel userNameLabel = new JLabel(userName);
+            JPanel userPanel = new JPanel(new GridLayout(1,2));
+            userPanel.add(profile);
+            userPanel.add(userNameLabel);
+            userPanel.setBorder(BorderFactory.createEtchedBorder());
+            contactsPanel.add(userPanel);
+        }
 
-        return contactsPannel;
+        return contactsPanel;
     }
 
     public static void main(String[] args) {
 
         JFrame rootPane = rootPane();
+        String[] nutzernamen = {"Max Mustermann","Patrick Mischka", "Michael Angermeier", "Aaron Schweig","Troy Kessler","Matthias von End", "Jan Grübener"};
+        Message firstMessage = new Message();
+        firstMessage.setContent("Hello World!");
+        firstMessage.setOrigin(UUID.randomUUID());
 
-        rootPane.getContentPane().add(displayRecentContacts());
+        rootPane.getContentPane().add(displayRecentContacts(nutzernamen));
 
-        rootPane.getContentPane().add(header("Max Mustermann"), BorderLayout.NORTH);
-        rootPane.getContentPane().add(displayMessage("Hallo"), BorderLayout.WEST);
-        rootPane.getContentPane().add(displayMessage("Hi wie gehts?"), BorderLayout.EAST);
+        /*rootPane.getContentPane().add(header("Max Mustermann"), BorderLayout.NORTH);
+        rootPane.getContentPane().add(displayNewMessage(firstMessage));
         rootPane.getContentPane().add(footer(), BorderLayout.SOUTH);
 
+         */
+
         rootPane.pack();
+
     }
+
 }
