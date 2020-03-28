6c9f95f659dfec7785291be457731dd7c621c5a1
Aaron Schweig <aaron.schweig@gmail.com>
2020-03-28 12:44:30 +0100

    Not cool, but working

--- src/main/java/vs/chat/entities/User.java
+++ src/main/java/vs/chat/entities/User.java
@@ -1,48 +1,77 @@
 package vs.chat.entities;
 
-import java.util.UUID;
-
 import vs.chat.server.warehouse.Warehouseable;
+
+import java.util.UUID;
 
 public class User implements Comparable<User>, Warehouseable {
 
-	private final UUID id;
-	private String username;
-	private transient String password;
-	
-	public User() {
-		this.id = UUID.randomUUID();
-	}
+    private final UUID id;
+    private String username;
+    //private byte[] password;
+    private int password;
 
-	public String getUsername() {
-		return username;
-	}
+    public User() {
+        this.id = UUID.randomUUID();
+    }
 
-	public void setUsername(String username) {
-		this.username = username;
-	}
+    public String getUsername() {
+        return username;
+    }
 
-	public String getPassword() {
-		return password;
-	}
+    public void setUsername(String username) {
+        this.username = username;
+    }
 
-	public void setPassword(String password) {
-		this.password = password;
-	}
-	
-	@Override
-	public boolean equals(Object obj) {
-		return this.id.equals(obj);
-	}
+    /*public String getPassword() {
+        //return new String(password, Charset.defaultCharset());
+    }*/
 
-	@Override
-	public UUID getId() {
-		return id;
-	}
+    public boolean hasPassword(String password) {
+        /*byte[] encodedPassword = this.encodePassword(password);
+        for (int i = 0; i < encodedPassword.length; i++) {
+            if (encodedPassword[i] != this.password[i]) {
+                return false;
+            }
+        }
+        return true;*/
+        return this.password == password.hashCode();
+    }
 
-	@Override
-	public int compareTo(final User o) {
-		return this.id.compareTo(o.id);
-	}
+    public void setPassword(String password) {
+        /*SecureRandom random = new SecureRandom();
+        byte[] salt = new byte[16];
+        random.nextBytes(salt);
+        */
+        this.password = this.encodePassword(password);
+    }
+
+    private int encodePassword(String password) {
+        // Salt von environment
+        /*KeySpec spec = new PBEKeySpec(password.toCharArray(), new byte[]{1, 2, 3, 4, 5}, 65536, 128);
+        try {
+            SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
+            return factory.generateSecret(spec).getEncoded();
+        } catch (NoSuchAlgorithmException | InvalidKeySpecException e) {
+            e.printStackTrace();
+        }
+        return new byte[0];*/
+        return password.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return this.id.equals(obj);
+    }
+
+    @Override
+    public UUID getId() {
+        return id;
+    }
+
+    @Override
+    public int compareTo(final User o) {
+        return this.id.compareTo(o.id);
+    }
 
 }
--- src/main/java/vs/chat/server/listener/LoginListener.java
+++ src/main/java/vs/chat/server/listener/LoginListener.java
@@ -1,52 +1,62 @@
 package vs.chat.server.listener;
 
 import java.io.IOException;
+import java.lang.reflect.Executable;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import vs.chat.entities.Chat;
 import vs.chat.entities.User;
 import vs.chat.packets.LoginPacket;
 import vs.chat.packets.LoginSyncPacket;
+import vs.chat.packets.NoOpPacket;
+import vs.chat.packets.Packet;
 import vs.chat.server.ConnectionHandler;
 import vs.chat.server.ServerContext;
 import vs.chat.server.warehouse.WarehouseResourceType;
+import vs.chat.server.warehouse.Warehouseable;
 
-public class LoginListener implements Listener<LoginPacket, LoginSyncPacket> {
 
-	@Override
-	public LoginSyncPacket next(final LoginPacket packet, final ServerContext context, final ConnectionHandler handler)
-			throws IOException {
-		// TODO Password / User prüfen
-		System.out.println("Invoked LoginListener");
+public class LoginListener implements Listener<LoginPacket, Packet> {
 
-		context.getWarehouse().get(WarehouseResourceType.USERS).values().stream()
-				.filter(u -> ((User) u).getUsername().equals(packet.username)).findFirst()
-				.ifPresentOrElse(storedUser -> {
-					var id = storedUser.getId();
-					handler.setConnectedToUserId(id);
-					System.out.println("connected id: " + id);
-				}, () -> {
-					System.out.println("writing new user");
-					var user = new User();
-					var id = user.getId();
-					user.setUsername(packet.username);
-					user.setPassword(packet.password);
+    @Override
+    public Packet next(final LoginPacket packet, final ServerContext context, final ConnectionHandler handler)
+            throws IOException {
+        // TODO Password / User prüfen
+        System.out.println("Invoked LoginListener");
 
-					context.getWarehouse().get(WarehouseResourceType.USERS).put(id, user);
-					System.out.println("created user with id:" + id);
-					context.getBroadcaster().send(packet);
-					handler.setConnectedToUserId(id);
-				});
+        var res = context.getWarehouse().get(WarehouseResourceType.USERS).values().stream()
+                .filter(u -> ((User) u).getUsername().equals(packet.username))
+                .findFirst();
+        if (res.isPresent()) {
+            if (!((User) res.get()).hasPassword(packet.password)) {
+                return new NoOpPacket();
+            } else {
+                var id = res.get().getId();
+                handler.setConnectedToUserId(id);
+                System.out.println("connected id: " + id);
+            }
+        } else {
+            System.out.println("writing new user");
+            var user = new User();
+            var id = user.getId();
+            user.setUsername(packet.username);
+            user.setPassword(packet.password);
 
-		var syncPacket = new LoginSyncPacket();
-		syncPacket.userId = handler.getConnectedToUserId().get();
-		syncPacket.chats = context.getWarehouse().get(WarehouseResourceType.CHATS).values().stream()
-				.map(chat -> (Chat) chat).filter(chat -> chat.getUsers().contains(syncPacket.userId))
-				.collect(Collectors.toSet());
-		syncPacket.users = context.getWarehouse().get(WarehouseResourceType.USERS).values().stream()
-				.map(user -> (User) user).collect(Collectors.toSet());
+            context.getWarehouse().get(WarehouseResourceType.USERS).put(id, user);
+            System.out.println("created user with id:" + id);
+            context.getBroadcaster().send(packet);
+            handler.setConnectedToUserId(id);
+        }
+        var syncPacket = new LoginSyncPacket();
+        syncPacket.userId = handler.getConnectedToUserId().get();
+        syncPacket.chats = context.getWarehouse().get(WarehouseResourceType.CHATS).values().stream()
+                .map(chat -> (Chat) chat).filter(chat -> chat.getUsers().contains(syncPacket.userId))
+                .collect(Collectors.toSet());
+        syncPacket.users = context.getWarehouse().get(WarehouseResourceType.USERS).values().stream()
+                .map(user -> (User) user).collect(Collectors.toSet());
 
-		return syncPacket;
-	}
+        return syncPacket;
+    }
 
 }
